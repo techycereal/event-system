b450e13d74ac642875f1e0a005567a8bc9fb09d9
techycereal <alexjmilliken@outlook.com>
2024-08-26 12:49:21 -0400

    middleware remove

--- server.js
+++ server.js
@@ -8,19 +8,12 @@ const databaseRoutes = require('./databaseRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json()); // Parse incoming JSON requests
 
 // Routes
 app.use('/api/google', googleRoutes); // All Google-related routes
 app.use('/api/database', databaseRoutes); // All database-related routes
-
-// Error Handling Middleware (optional but recommended)
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-});
 
 // Start the server
 app.listen(PORT, () => {
